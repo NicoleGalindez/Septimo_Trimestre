3a2d95189f0c8343c4faaa5163f7e51605a1206d
angelasofia9 <ascampos8@misena.edu.co>
2024-03-05 17:23:28 -0500

    info

--- material-tailwind-kit-react-main/material-tailwind-kit-react-main/src/data/features-data.js
+++ material-tailwind-kit-react-main/material-tailwind-kit-react-main/src/data/features-data.js
@@ -10,21 +10,21 @@ export const featuresData = [
     title: "Como funciona nuestro aplicativo",
     icon:   CogIcon,
     description:
-      "Divide details about your product or agency work into parts. A paragraph describing a feature will be enough.",
+      "Nuestra aplicación ofrece control parental mediante rutas georreferenciadas. El padre crea y monitorea las rutas, mientras que el menor las sigue desde su dispositivo. Si se desvía, el padre recibe una notificación instantánea.",
   },
   {
     color: "gray",
-    title: "seguridad",
+    title: "Seguridad proporcionada por nuestro aplicativo",
     icon: ShieldCheckIcon,
     description:
-      "Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious.",
+      "En nuestro aplicativo, la seguridad es nuestra prioridad principal. Utilizamos tecnologia de vanguardia para garantizar la proteccion de tu hijo en todo momento. Con la capacidad de rastrear la ubicación en tiempo real y recibir notificaciones instantáneas si se desvía de las rutas establecidas, proporcionamos una capa adicional de seguridad y tranquilidad para los padres. Nuestro compromiso es brindarte una experiencia segura y confiable mientras tu hijo explora el mundo.",
   },
   {
     color: "gray",
-    title: " Alertas",
+    title: "Notificación de alerta ante desviaciones de ruta",
     icon: BellAlertIcon,
     description:
-      "Recibe un mensaje SOS del niño cuando se encuentra en peligro y él no pueda llamarte. Tambien recibiras su ubicación exacta.",
+      "En caso de que tu hijo se desvíe de la ruta asignada, nuestra aplicación te notificará al instante para que puedas tomar medidas rápidas y efectivas. Tu tranquilidad es nuestra prioridad, por eso te mantendremos informado en tiempo real sobre cualquier cambio inesperado en el recorrido de tu hijo. Con esta función, puedes estar seguro de que siempre estarás al tanto de la ubicación y seguridad de tu hijo, incluso cuando se aleje de las rutas establecidas.",
   },
 ];
 
